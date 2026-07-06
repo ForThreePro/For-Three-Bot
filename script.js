c3070f015afd168530c10c60595a0a2ad3d57f75
YALLICO OWNER <yallico2024@gmail.com>
2026-07-06 14:05:38 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-const btnTema = document.getElementById('btnTema');
-const body = document.body;
-
-window.addEventListener('DOMContentLoaded', () => {
-    if(localStorage.getItem('tema') === 'light'){
-        body.classList.add('light-mode');
-        btnTema.innerText = '☀️';
-    } else {
-        btnTema.innerText = '🌙';
-    }
-});
-
-btnTema.addEventListener('click', () => {
-    body.classList.toggle('light-mode');
-    if(body.classList.contains('light-mode')){
-        localStorage.setItem('tema', 'light');
-        btnTema.innerText = '☀️'; 
-    } else {
-        localStorage.setItem('tema', 'dark');
-        btnTema.innerText = '🌙';
-    }
-});
+const btnTema=document.getElementById('btnTema');const body=document.body;
+window.addEventListener('DOMContentLoaded',()=>{if(localStorage.getItem('tema')==='light'){body.classList.add('light-mode');btnTema.innerText='☀️';}else{btnTema.innerText='🌙';}});
+btnTema.addEventListener('click',()=>{body.classList.toggle('light-mode');if(body.classList.contains('light-mode')){localStorage.setItem('tema','light');btnTema.innerText='☀️';}else{localStorage.setItem('tema','dark');btnTema.innerText='🌙';}});
