421c12588511dc63d8e0d1f2a3ee53e96afb5416
YALLICO OWNER <yallico2024@gmail.com>
2026-07-08 11:54:27 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function cambiarPais(){
   if(pais=='MXN'){s='$';pf=precio*5.5}if(pais=='CLP'){s='$';pf=precio*1000}if(pais=='COP'){s='$';pf=precio*1300}
   document.getElementById('btnPais').innerText='PAÍS: '+pais;
   document.getElementById('precio').innerText=s+Math.round(pf).toLocaleString();
-  document.getElementById('yapeBox').style.display=pais=='PEN'?'flex':'none';
+  document.getElementById('yapeItem').style.display=pais=='PEN'?'flex':'none';
 }
 function cerrarPago(){document.getElementById('popup').style.display='none'}
-function copiar(id){navigator.clipboard.writeText(document.getElementById(id).innerText);alert('Copiado')}
+function copiar(id){navigator.clipboard.writeText(document.getElementById(id).innerText);alert('✅ Copiado')}
 const m=document.getElementById('musica');m.volume=0.2;let ini=false;
 document.body.onclick=()=>{if(!ini){m.play();ini=true}};
 function toggleMusica(){const b=document.getElementById('btnMusica');if(m.paused){m.play();b.innerText='🔊'}else{m.pause();b.innerText='🔇'}}
