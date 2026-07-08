a53a19c27cf07d0ae603b4e84dc5c96363ca4979
YALLICO OWNER <yallico2024@gmail.com>
2026-07-08 11:27:21 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let pais = 'PEN';
+let precioBase = 0;
+
+function abrirSelectorPais(){ 
+  document.getElementById('selectorPais').focus(); 
+}
+
+function abrirPago(nombre, precio){ 
+  precioBase = precio;
+  document.getElementById('popupPago').style.display = 'flex';
+  document.getElementById('nombrePlan').innerText = nombre;
+  cambiarPais();
+}
+
 function cambiarPais(){
   pais = document.getElementById('selectorPais').value;
   let simbolo = 'S/';
@@ -8,5 +22,31 @@ function cambiarPais(){
   if(pais === 'COP'){ simbolo = '$'; precioFinal = precioBase * 1300; }
   
   document.getElementById('btnPais').innerText = '⚡ País: ' + pais;
-  document.getElementById('precioConvertido').innerText = simbolo + Math.round(precioFinal);
+  document.getElementById('precioConvertido').innerText = simbolo + Math.round(precioFinal).toLocaleString();
+}
+
+function cerrarPago(){ 
+  document.getElementById('popupPago').style.display = 'none'; 
+}
+
+const musica = document.getElementById('musicaFondo'); 
+musica.volume = 0.3;
+let musicaIniciada = false;
+
+document.body.addEventListener('click', () => { 
+  if(!musicaIniciada){ 
+    musica.play().catch(err => {}); 
+    musicaIniciada = true; 
+  } 
+});
+
+function toggleMusica(){ 
+  const btn = document.getElementById('btnMusica'); 
+  if(musica.paused){ 
+    musica.play(); 
+    btn.innerText = '🔊'; 
+  } else { 
+    musica.pause(); 
+    btn.innerText = '🔇'; 
+  } 
 }
