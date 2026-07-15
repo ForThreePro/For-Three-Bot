52c18c8c60a809aad1cf01ec3154eff014a37bdb
YALLICO OWNER <yallico2024@gmail.com>
2026-07-15 12:39:17 -0500

    Actualizar music.js

--- music.js
+++ music.js
@@ -3,24 +3,24 @@ const playlist = [
     title: "After Conep",
     artist: "Whois Yallico",
     src: "Musica.mp3",
-    cover: "https://placehold.co/250x250/A78BFA/0D0B14?text=After"
+    cover: "https://placehold.co/300x300/A78BFA/000?text=After"
   },
   {
     title: "Tu Me Enamoraste Remix",
     artist: "Whois Yallico",
     src: "Musica1.mp3",
-    cover: "https://placehold.co/250x250/EC4899/0D0B14?text=Remix"
+    cover: "https://placehold.co/300x300/EC4899/000?text=Remix"
   },
   {
     title: "Talento Yan Block",
     artist: "Whois Yallico",
     src: "Musica2.mp3",
-    cover: "https://placehold.co/250x250/8B5CF6/0D0B14?text=Talento"
+    cover: "https://placehold.co/300x300/8B5CF6/000?text=Talento"
   },
   {
     title: "Piensas En Mi",
     artist: "Whois Yallico",
     src: "Musica3.mp3",
-    cover: "https://placehold.co/250x250/A78BFA/0D0B14?text=Piensas"
+    cover: "https://placehold.co/300x300/1DB954/000?text=Piensas"
   }
 ];
