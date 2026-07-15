4b82d2031582187da08ca80fdb149c47db9c914d
YALLICO OWNER <yallico2024@gmail.com>
2026-07-15 02:15:22 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,40 +1,18 @@
-// CONTROL MUSICA SPOTIFY
+function openPopup(product, price) {
+  document.getElementById('popupProduct').innerText = product;
+  document.getElementById('popupPrice').innerText = price;
+  let m = `Hola quiero comprar: ${product} - ${price}`;
+  document.getElementById('btnYape').href = `https://wa.me/51936994155?text=${m} Yape`;
+  document.getElementById('btnPrex').href = `https://wa.me/51936994155?text=${m} Prex`;
+  document.getElementById('btnGlobal').href = `https://wa.me/51936994155?text=${m} Global66`;
+  document.getElementById('paymentPopup').classList.add('active');
+}
+function closePopup() {document.getElementById('paymentPopup').classList.remove('active')}
+
 let audio = document.getElementById('bgMusic');
 let playBtn = document.getElementById('playBtn');
-let progressBar = document.getElementById('progressBar');
-let currentTime = document.getElementById('currentTime');
-let duration = document.getElementById('duration');
-
 function toggleMusic() {
-  if(audio.paused){
-    audio.play();
-    playBtn.innerText = '⏸'
-  } else {
-    audio.pause();
-    playBtn.innerText = '▶'
-  }
+  if(audio.paused){audio.play();playBtn.innerText = '⏸'} 
+  else {audio.pause();playBtn.innerText = '▶'}
 }
-
-audio.addEventListener('timeupdate', ()=>{
-  if(!isNaN(audio.duration)){
-    progressBar.value = (audio.currentTime/audio.duration)*100;
-    currentTime.innerText = format(audio.currentTime)
-  }
-})
-
-audio.addEventListener('loadedmetadata', ()=>{
-  duration.innerText = format(audio.duration)
-})
-
-progressBar.oninput = ()=>{
-  audio.currentTime = (progressBar.value/100)*audio.duration
-}
-
-function format(s){
-  if(isNaN(s)) return "0:00";
-  let m = Math.floor(s/60);
-  let sec = Math.floor(s%60);
-  return `${m}:${sec<10?'0':''}${sec}`
-}
-
-audio.volume = 0.3; // volumen bajito
+audio.volume = 0.3;
