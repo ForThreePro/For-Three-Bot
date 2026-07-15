05720bbc7a21de198bb4952ad0dece7416104012
YALLICO OWNER <yallico2024@gmail.com>
2026-07-15 10:46:15 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-// BUSCADOR COMANDOS
+// BUSCADOR
 document.getElementById('commandSearch').addEventListener('input', e=>{
   let s = e.target.value.toLowerCase();
-  document.querySelectorAll('.cmd-tags span').forEach(c=>{
-    c.style.display = c.innerText.toLowerCase().includes(s) ? 'inline-block' : 'none'
+  document.querySelectorAll('.cmd-item').forEach(c=>{
+    c.style.display = c.innerText.toLowerCase().includes(s) ? 'block' : 'none'
   });
 });
 
-// POPUP COMPRA
+// POPUP CON TUS DATOS
+let productoActual = "";
+let precioActual = "";
+
 function openPopup(product, price) {
+  productoActual = product;
+  precioActual = price;
   document.getElementById('popupProduct').innerText = product;
   document.getElementById('popupPrice').innerText = price;
-  let m = `Hola quiero comprar: ${encodeURIComponent(product)} - ${encodeURIComponent(price)}`;
-  document.getElementById('btnYape').href = `https://wa.me/51936994155?text=${m} Yape`;
-  document.getElementById('btnPrex').href = `https://wa.me/51936994155?text=${m} Prex`;
-  document.getElementById('btnGlobal').href = `https://wa.me/51936994155?text=${m} Global66`;
+  
+  let mensaje = `Hola! Quiero comprar:%0A*Producto:* ${product}%0A*Precio:* ${price}%0A%0A*Ya realicé el pago*%0AAdjunto captura`;
+  document.getElementById('btnConfirmar').href = `https://wa.me/51936994155?text=${mensaje}`;
+  
   document.getElementById('paymentPopup').classList.add('active');
   document.body.style.overflow = 'hidden';
 }
@@ -25,16 +30,11 @@ document.getElementById('paymentPopup').addEventListener('click', e=>{
   if(e.target.id === 'paymentPopup') closePopup()
 })
 
-// PLAYER MUSICA
+// MUSICA
 let audio = document.getElementById('bgMusic');
 let playBtn = document.getElementById('playBtn');
 function toggleMusic() {
-  if(audio.paused){
-    audio.play();
-    playBtn.innerText = '⏸'
-  } else {
-    audio.pause();
-    playBtn.innerText = '▶'
-  }
+  if(audio.paused){audio.play();playBtn.innerText = '⏸'} 
+  else {audio.pause();playBtn.innerText = '▶'}
 }
-audio.volume = 0.3;
+audio.volume = 0.45;
