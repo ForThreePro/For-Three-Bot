fa6796543ee11f911160a5463fa8b766ab2272a9
YALLICO OWNER <yallico2024@gmail.com>
2026-07-17 20:42:00 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -51,4 +51,4 @@ document.getElementById('formPedido').addEventListener('submit', (e) => {
 });
 
 let contador = 3; setInterval(() => { if(contador > 1) contador--; document.getElementById('contador').textContent = contador; }, 10000);
-document.querySelectorAll('a[href^="#"]').forEach(anchor => { anchor.addEventListener('click', function (e) { e.preventDefault(); document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' }); }); });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => { anchor.addEventListener('click', function (e) { e.preventDefault(); document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' }); });
